59bbec0ba91e286712e8881385ff57c1408290c2
violetflare@protonmail.com <violetflare@protonmail.com>
2021-10-15 22:59:27 +0200

    fix

--- sounds.js
+++ sounds.js
@@ -5,5 +5,6 @@ module.exports = [
     {   ghost_damaged: fs.readFileSync('./sounds/ghost_damaged.mp3')    },
     {   ghost_light_attack: fs.readFileSync('./sounds/ghost_light_attack.mp3')  },
     {   chains_rattle: fs.readFileSync('./sounds/chains_rattle.mp3')    },
-    {   piano: fs.readFileSync('./sounds/piano.mp3')    }
+    {   piano: fs.readFileSync('./sounds/piano.mp3')    },
+    {   banshee_scream: fs.readFileSync('./sounds/banshee_scream.mp3')    }
 ];
